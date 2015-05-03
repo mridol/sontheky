d0ec380a84c1140d3ffa555e1378c0df8adcc27c
brotherhood2014 <vnknowledge2014@gmail.com>
2015-05-03 16:19:23 +0700

    bak

--- js/madeJS.js
+++ js/madeJS.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
 	$(function(){
 		var width = $(window).width(), height = $(window).height();
-		if (width <= 768) {
+		if (width <= 800) {
 			$('.nav').addClass('nav-small-screen').removeClass('nav');
 			$('.nav-small-screen').hide();
 			$('.button-black').addClass('button-alice').removeClass('button-black');
@@ -19,18 +19,39 @@ jQuery(document).ready(function($) {
 		        }
 		        else {
 		            $('.nav-small-screen').show();
-		            $('.nav-small-screen').animate({ 'width': '240px' }, 'slow');
 		            $('.menu-icon').removeClass('fa-bars').addClass('fa-times');
 		            $('.menu-icon').addClass('menu-icon-close').removeClass('menu-icon');
-		            $('.menu-icon-close').animate({ 'margin-left': '240px' }, 'slow');
-		            $('.wrapper-section').animate({ 'margin-left': '240px' }, 'slow');
+		            if (width > 480){
+		            	$('.nav-small-screen').animate({ 'width': '240px' }, 'slow');
+		            	$('.menu-icon-close').animate({ 'margin-left': '240px' }, 'slow');
+		            	$('.wrapper-section').animate({ 'margin-left': '240px' }, 'slow');
+		            }
+		            else{
+		            	$('.nav-small-screen').animate({ 'width': '120px' }, 'slow');
+			            $('.menu-icon-close').animate({ 'margin-left': '120px' }, 'slow');
+			            $('.wrapper-section').animate({ 'margin-left': '120px' }, 'slow');
+		            }	            
 		        }
 		    });
 		} else {
 			$('.nav-small-screen').addClass('nav').removeClass('nav-small-screen');
 			$('.button-alice').addClass('button-black').removeClass('button-alice');
-			$('.hvr-underline-from-center-alice').addClass('hvr-underline-from-center-black').removeClass('hvr-underline-from-center-alice');
-		}
+			$('.hvr-underline-from-center-alice').addClass('hvr-underline-from-center-black').removeClass('hvr-underline-from-center-alice');		
+		};
+
+		if(width >=1280){
+			var nav = $('.nav'), brand = $('.brand');
+    		$(window).scroll(function(){
+    			if($(this).scrollTop() > 100){
+    				nav.addClass('menuScroll slideInDown ani');
+    				brand.addClass('menuScroll slideInDown ani');
+    			}
+    			else if($(this).scrollTop() <= 100){
+    				brand.removeClass('menuScroll slideInDown ani');
+    				nav.removeClass('menuScroll slideInDown ani');
+    			};
+   			});
+		};
 	});
 });
 
